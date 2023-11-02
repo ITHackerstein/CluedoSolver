f38b1acdb3eec45cc6cbe66436a3220092048b40
ITHackerstein <carelladavide1@gmail.com>
2023-11-02 10:33:30 +0100

    Card: Created some card utilities.

---
+++ src/Card.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+namespace Cluedo {
+
+enum class CardCategory : std::uint8_t {
+	Suspect = 0,
+	Weapon = 6,
+	Room = 12
+};
+
+enum class Card : std::uint8_t {
+	// Suspects
+	Green,
+	Mustard,
+	Orchid,
+	Peacock,
+	Plum,
+	Scarlet,
+	// Weapons
+	Candlestick,
+	Knife,
+	Pipe,
+	Pistol,
+	Rope,
+	Wrench,
+	// Rooms
+	BallardRoom,
+	Ballroom,
+	DiningRoom,
+	Greenhouse,
+	Hall,
+	Kitchen,
+	Library,
+	Lounge,
+	Study,
+	// Used to count cards
+	_Count
+};
+
+struct CardUtils {
+	static constexpr std::array card_categories { CardCategory::Suspect, CardCategory::Weapon, CardCategory::Room };
+
+	struct CardIterator {
+		std::uint8_t index;
+		// cppcheck-suppress noExplicitConstructor
+		constexpr CardIterator(std::uint8_t i)
+		  : index(i) {}
+
+		constexpr std::uint8_t operator*() const { return index; }
+		constexpr bool operator!=(CardIterator const& other) const { return index != other.index; }
+		constexpr void operator++() { ++index; }
+	};
+
+	struct cards {
+		constexpr CardIterator begin() const { return 0; }
+		constexpr CardIterator end() const { return static_cast<std::uint8_t>(Card::_Count); }
+	};
+
+	struct cards_per_type {
+		CardCategory category;
+		explicit constexpr cards_per_type(CardCategory c)
+		  : category(c) {}
+
+		typedef std::uint8_t iterator;
+		constexpr CardIterator begin() const { return static_cast<std::uint8_t>(category); }
+		constexpr CardIterator end() const {
+			std::uint8_t count;
+			switch (category) {
+			case CardCategory::Suspect:
+				count = 6;
+				break;
+			case CardCategory::Weapon:
+				count = 6;
+				break;
+			case CardCategory::Room:
+				count = 12;
+				break;
+			}
+
+			return static_cast<std::uint8_t>(category) + count;
+		}
+	};
+};
+
+};
