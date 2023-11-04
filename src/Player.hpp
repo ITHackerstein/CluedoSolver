aeae3759fa137d31fa20b2c12e64eac33b359067
ITHackerstein <carelladavide1@gmail.com>
2023-11-04 16:52:39 +0100

    Player: Created a new player class.

---
+++ src/Player.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "Card.hpp"
+
+#include <optional>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+namespace Cluedo {
+
+class Player {
+public:
+	explicit Player(std::string const& name, std::size_t n_cards)
+	  : Player(name, n_cards, false) {}
+
+	explicit Player(std::string const& name, std::size_t n_cards, bool is_solution)
+	  : m_name(name), m_n_cards(n_cards), m_is_solution(is_solution) {}
+
+	std::string const& name() const { return m_name; }
+	std::size_t n_cards() const { return m_n_cards; }
+	bool is_solution() const { return m_is_solution; }
+
+	std::optional<bool> has_card(Card card) const {
+		if (m_cards_in_hand.contains(card))
+			return true;
+
+		if (m_cards_not_in_hand.contains(card))
+			return false;
+
+		return {};
+	}
+
+	// FIXME: Modifying any of these should try to "simplify" the possible cards array.
+	void add_in_hand_card(Card card) { m_cards_in_hand.insert(card); }
+	void add_not_in_hand_card(Card card) { m_cards_not_in_hand.insert(card); }
+	void add_possible_cards(std::unordered_set<Card> const& set) { m_possible_cards.push_back(set); }
+
+private:
+	std::string m_name;
+	std::size_t m_n_cards;
+	bool m_is_solution;
+
+	std::unordered_set<Card> m_cards_in_hand;
+	std::unordered_set<Card> m_cards_not_in_hand;
+	std::vector<std::unordered_set<Card>> m_possible_cards;
+};
+
+};
