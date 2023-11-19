0dfb075c3f82891e51166c9d07cb416c71f3aa94
ITHackerstein <carelladavide1@gmail.com>
2023-11-19 15:14:08 +0100

    Solver: Added getter for the number of players

--- src/Solver.hpp
+++ src/Solver.hpp
@@ -19,6 +19,8 @@ public:
 
 	static Result<Solver, Error> create(std::vector<PlayerData> const&);
 
+	std::size_t n_players() const { return m_players.size() - 1; }
+
 	Player const& player(std::size_t player_index) const { return m_players.at(player_index); }
 	Player& player(std::size_t player_index) { return m_players.at(player_index); }
 
