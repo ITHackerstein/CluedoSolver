2a0c7180dbaddbe0069d1d114dda41553160abab
ITHackerstein <carelladavide1@gmail.com>
2023-11-21 12:28:52 +0100

    CardSet: Using operator[] when indexing with Card because in that case there's no need to check the index, we know it's safe.

--- src/CardSet.hpp
+++ src/CardSet.hpp
@@ -24,7 +24,7 @@ public:
 		}
 
 		constexpr void advance_to_next_index() {
-			while (m_index < m_set_ref.size() && !m_set_ref.test(m_index))
+			while (m_index < m_set_ref.size() && !m_set_ref[m_index])
 				++m_index;
 		}
 
@@ -54,17 +54,17 @@ public:
 
 	constexpr std::size_t size() const { return m_set.count(); }
 	constexpr bool empty() const { return size() == 0; }
-	constexpr bool contains(Card card) const { return m_set.test(static_cast<std::size_t>(card)); }
+	constexpr bool contains(Card card) const { return m_set[static_cast<std::size_t>(card)]; }
 
 	constexpr bool insert(Card card) {
 		if (contains(card))
 			return true;
 
-		m_set.set(static_cast<std::size_t>(card));
+		m_set[static_cast<std::size_t>(card)] = true;
 		return false;
 	}
 
-	constexpr void erase(Card card) { m_set.reset(static_cast<std::size_t>(card)); }
+	constexpr void erase(Card card) { m_set[static_cast<std::size_t>(card)] = false; }
 	constexpr void clear() { m_set.reset(); }
 
 	constexpr bool operator==(CardSet const& other) const = default;
